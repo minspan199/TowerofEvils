969a28eb7e8d3b524b7a80f6b7e49bde1a3bac68
minspan1999 <minspan@users.noreply.github.com>
2020-05-23 14:57:25 -0400

    update dependency

--- app/src/main/java/com/michael/pan/eviltower/views/BattleView.java
+++ app/src/main/java/com/michael/pan/eviltower/views/BattleView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.AttributeSet;
